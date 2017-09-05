4451cc18282158244bc9de11cc6ee114b7880715
nzl <hlw123456>
2017-09-05 15:44:42 +0800

    xps更新excel和ppt

--- api/src/test/java/com/liumapp/api/worker/WatcherTest.java
+++ api/src/test/java/com/liumapp/api/worker/WatcherTest.java
@@ -9,73 +9,61 @@ import java.io.IOException;
 /**
  * Created by liumapp on 9/1/17.
  * E-mail:liumapp.com@gmail.com
- * home-page:http://www.liumapp.com
+ * home-page:http: www.liumapp.com
  */
-public class WatcherTest extends TestCase {
-
-    private Queen queen;
-<<<<<<< HEAD
-=======
-
->>>>>>> 7eaab489d39af141d6509f41ccd4cf10bd004855
-    @Ignore
-    public void testOrder () {
-        try {
-            queen.say("give me a word");
-            System.out.println(queen.hear());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-<<<<<<< HEAD
-=======
-
->>>>>>> 7eaab489d39af141d6509f41ccd4cf10bd004855
-    @Ignore
-    public void testOpenOffice() {
-        queen.say("http://oss.aliyun.com/test_testfile_ppt_5172912");
-        try {
-            System.out.println(queen.hear());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-<<<<<<< HEAD
-=======
-
->>>>>>> 7eaab489d39af141d6509f41ccd4cf10bd004855
-    @Ignore
-    public void testXps() {
-        queen.say("http://oss.aliyun.com/test_testfile_doc_5172912");
-        try {
-            System.out.println(queen.hear());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-<<<<<<< HEAD
-    public void testWorkInvo() {
-=======
-    @Ignore
-    public void xpsWorkInvo() {
->>>>>>> 7eaab489d39af141d6509f41ccd4cf10bd004855
-    	queen.say("D:/Demo/office2pdf/data_test0_docx_218876");
-        try {
-            System.out.println(queen.hear());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    @Ignore
-    @Override
-    protected void setUp() throws Exception {
-
-        queen = new Queen();
-        queen.connect();
-
-    }
-
-
-}
+ public class WatcherTest extends TestCase {
+ 
+     private Queen queen;
+ 
+ 
+     @Ignore
+     public void testOrder () {
+         try {
+             queen.say("give me a word");
+             System.out.println(queen.hear());
+         } catch (IOException e) {
+             e.printStackTrace();
+         }
+     }
+ 
+     @Ignore
+     public void testOpenOffice() {
+         queen.say("http: oss.aliyun.com/test_testfile_ppt_5172912");
+         try {
+             System.out.println(queen.hear());
+         } catch (IOException e) {
+             e.printStackTrace();
+         }
+     }
+ 
+     @Ignore
+     public void testXps() {
+         queen.say("http: oss.aliyun.com/test_testfile_doc_5172912");
+         try {
+             System.out.println(queen.hear());
+         } catch (IOException e) {
+             e.printStackTrace();
+         }
+     }
+ 
+     @Ignore
+     public void testWorkInvo() {//D:/Demo/office2pdf/data_test0_docx_218876
+             queen.say("C:/office2pdf/data/test0.docx");
+             try {
+                 System.out.println(queen.hear());
+             } catch (IOException e) {
+                 e.printStackTrace();
+             }
+         }
+ 
+ 
+         @Override
+         protected void setUp () throws Exception {
+ 
+             queen = new Queen();
+             queen.setAddress("118.190.136.193");
+             queen.connect();
+ 
+         }
+ 
+ }
--- xps/src/main/java/com/liumapp/xps/utils/Xps2pdf.java
+++ xps/src/main/java/com/liumapp/xps/utils/Xps2pdf.java
@@ -1,6 +1,7 @@
 package com.liumapp.xps.utils;
 
 import java.io.File;
+import java.util.Date;
 
 import com.jacob.activeX.ActiveXComponent;
 import com.jacob.com.ComThread;
@@ -58,6 +59,77 @@ public class Xps2pdf {
 	            }
 	}
 
+	/**
+	 *利用xps将excel文档转换成pdf格式
+	 * @param inFilePath  文件路径
+	 * @param outFilePath 转换后的文件保存路径
+	 * @return
+	 */
+	public  String xls2Pdf(String inFilePath,String outFilePath){
+		ComThread.InitSTA(true);
+		ActiveXComponent ax=new ActiveXComponent("Excel.Application");
+		try{
+			ax.setProperty("Visible", new Variant(false));
+			ax.setProperty("AutomationSecurity", new Variant(3)); //禁用宏
+			Dispatch excels=ax.getProperty("Workbooks").toDispatch();
+
+			Dispatch excel=Dispatch.invoke(excels,"Open",Dispatch.Method,new Object[]{
+							inFilePath,
+							new Variant(false),
+							new Variant(false)
+					},
+					new int[9]).toDispatch();
+			//转换格式
+			Dispatch.invoke(excel,"ExportAsFixedFormat",Dispatch.Method,new Object[]{
+					new Variant(0), //PDF格式=0
+					outFilePath,
+					new Variant(0)  //0=标准 (生成的PDF图片不会变模糊) 1=最小文件 (生成的PDF图片糊的一塌糊涂)
+			},new int[1]);
+
+			Dispatch.call(excel, "Close",new Variant(false));
+
+			if(ax!=null){
+				ax.invoke("Quit",new Variant[]{});
+				ax=null;
+			}
+			ComThread.Release();
+			return "";
+		}catch(Exception es){
+			return es.toString();
+		}
+	}
+
+	/**
+	 * 将ppt文件转换成pdf格式
+	 * @param inputFile 文件的路径
+	 * @param pdfFile  转换成功后的文件保存路径
+	 * @return
+	 */
+	private  boolean ppt2PDF(String inputFile, String pdfFile) {
+		try {
+			ComThread.InitSTA(true);
+			ActiveXComponent app = new ActiveXComponent("KWPP.Application");
+			//            app.setProperty("Visible", false);
+			System.out.println("开始转化PPT为PDF...");
+			long date = new Date().getTime();
+			Dispatch ppts = app.getProperty("Presentations").toDispatch();
+			Dispatch ppt = Dispatch.call(ppts, "Open", inputFile, true, // ReadOnly
+					//    false, // Untitled指定文件是否有标题
+					false// WithWindow指定文件是否可见
+			).toDispatch();
+			Dispatch.invoke(ppt, "SaveAs", Dispatch.Method, new Object[]{
+					pdfFile,new Variant(32)},new int[1]);
+			System.out.println("PPT");
+			Dispatch.call(ppt, "Close");
+			long date2 = new Date().getTime();
+			int time = (int) ((date2 - date) / 1000);
+			app.invoke("Quit");
+			return true;
+		} catch (Exception e) {
+			// TODO: handle exception
+			return false;
+		}
+	}
 	public String xpsWork (Orderpattern orderpattern) {		
 		String sfileName = orderpattern.getSavePath()+"/"+orderpattern.getFileName()+"."+orderpattern.getType();
 		String toFileName = orderpattern.getSavePath()+"/"+orderpattern.getFileName()+".pdf";
